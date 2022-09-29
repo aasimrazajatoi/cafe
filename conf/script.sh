1a1f2631cb9e0f6930e3fefab211fcf57f76fe80
Aasim <aasimraza.ddevops@gmail.com>
2022-09-29 19:05:52 +0500

    new commit

--- conf/script.sh
+++ conf/script.sh
@@ -1,23 +1,23 @@
-!#/bin/bash
+#!/bin/bash
 
-#copy code from workspace to nginx server
-scp -r -i new.pem * ubuntu@50.18.76.214:/home/ubuntu/cafe
+echo "copy code from workspace to nginx server..."
+scp -r -i ../new.pem * ubuntu@50.18.76.214:/home/ubuntu/cafe
 
-#copy code to /var/www/
-ssh -i new.pem ubuntu@50.18.76.214 "sudo cp -r /home/ubuntu/cafe /var/www/"
+echo "copy code to /var/www/"
+ssh -i ../new.pem ubuntu@50.18.76.214 "sudo cp -r /home/ubuntu/cafe /var/www/"
 
-#copy virtual host file
-ssh -i new.pem ubuntu@50.18.76.214 "sudo cp -r /home/ubuntu/cafe/conf/cafe /etc/nginx/sites-available/"
+echo "copy virtual host file..."
+ssh -i ../new.pem ubuntu@50.18.76.214 "sudo cp -r /home/ubuntu/cafe/conf/cafe /etc/nginx/sites-available/"
 
-#remove code
-ssh -i new.pem ubuntu@50.18.76.214 "rm -rf /home/ubuntu/cafe"
+echo "remove code..."
+ssh -i ../new.pem ubuntu@50.18.76.214 "rm -rf /home/ubuntu/cafe"
 
-#make symbolic link
-ssh -i new.pem ubuntu "sudo ln -s /etc/nginx/sites-available/cafe /etc/nginx/sites-enabled/cafe"
+echo "make symbolic link..."
+ssh -i ../new.pem ubuntu "sudo ln -s /etc/nginx/sites-available/cafe /etc/nginx/sites-enabled/cafe"
 
 
-#reload nginx
-ssh -i new.pem ubuntu@50.18.76.214 "sudo systemctl reload nginx"
+echo "reload nginx..."
+ssh -i ../new.pem ubuntu@50.18.76.214 "sudo systemctl reload nginx"
 
 
 
